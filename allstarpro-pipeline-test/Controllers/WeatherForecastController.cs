401455e57a080df80f92d5024141a0508fb6c4f0
Mahammad Agayev <mahammad.agayev@simbrella.com>
2022-09-05 19:28:34 +0400

    decreased weather options

--- allstarpro-pipeline-test/Controllers/WeatherForecastController.cs
+++ allstarpro-pipeline-test/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace allstarpro_pipeline_test.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing"//, "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
